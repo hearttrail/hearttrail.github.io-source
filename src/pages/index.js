f6c954ae42883e2f1fb5984029f04bcbd9343cfa
Nic Gordon <nic.gordon@inlight.com.au>
2018-12-10 23:50:56 +1100

    Add placeholder copy

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,11 +4,16 @@ import React from 'react';
 
 import ArticlePreview from '../components/article-preview';
 import Button from '../components/button';
+import ContentBlock from '../components/content-block';
 import constants from '../constants';
 import Hero from '../components/hero';
 import Map from '../components/map';
 import QuoteBlock from '../components/quote-block';
 import styles from './index.module.scss';
+
+import BikeCloseUpImage from '../images/bike-close-up.jpg';
+import JamesRecoveryImage from '../images/james-recovery.jpg';
+import JamesSiloImage from '../images/james-sheppard.jpg';
 
 class RootIndex extends React.Component {
   render() {
@@ -39,6 +44,53 @@ class RootIndex extends React.Component {
           </Button>
         </QuoteBlock>
         <Map />
+        <div className="wrapper">
+          <img src={JamesSiloImage} alt="James in front of a silo" />
+        </div>
+        <div className="wrapper">
+          <ContentBlock
+            image={{
+              align: 'right',
+              alt: 'Bike with silo in background',
+              src: BikeCloseUpImage,
+            }}
+          >
+            Toad-like smile Flourish and Blotts he knew I’d come back Quidditch
+            World Cup. Fat Lady baubles banana fritters fairy lights Petrificus
+            Totalus. So thirsty, deluminator firs’ years follow me 12 inches of
+            parchment. Head Boy start-of-term banquet Cleansweep Seven roaring
+            lion hat. Unicorn blood crossbow mars is bright tonight, feast
+            Norwegian Ridgeback. Come seek us where our voices sound, we cannot
+            sing above the ground, Ginny Weasley bright red. Fanged frisbees,
+            phoenix tears good clean match. Toad-like smile Flourish and Blotts
+            he knew I’d come back Quidditch World Cup. Fat Lady baubles banana
+            fritters fairy lights Petrificus Totalus. So thirsty, deluminator
+            firs’ years follow me 12 inches of parchment. Head Boy start-of-term
+            banquet Cleansweep Seven roaring lion hat. Unicorn blood crossbow
+            mars is bright tonight, feast Norwegian Ridgeback. Come seek us
+            where our voices sound, we cannot sing above the ground, Ginny
+            Weasley bright red. Fanged frisbees, phoenix tears good clean match.
+          </ContentBlock>
+        </div>
+        <div className="wrapper">
+          <ContentBlock
+            image={{
+              align: 'left',
+              alt: 'James on the road to recovery',
+              src: JamesRecoveryImage,
+            }}
+          >
+            Half-giant jinxes peg-leg gillywater broken glasses large black dog
+            Great Hall. Nearly-Headless Nick now string them together, and
+            answer me this, which creature would you be unwilling to kiss?
+            Poltergeist sticking charm, troll umbrella stand flying cars golden
+            locket Lily Potter. Pumpkin juice Trevor wave your wand out glass
+            orbs, a Grim knitted hats. Stan Shunpike doe patronus, suck his soul
+            Muggle-Born large order of drills the trace. Bred in captivity fell
+            through the veil, quaffle blue flame ickle diddykins Aragog. Yer a
+            wizard, Harry Doxycide the woes of Mrs. Weasley Goblet of Fire.
+          </ContentBlock>
+        </div>
         <div className="wrapper">
           <h2 className="section-headline">Recent articles</h2>
           <ul className="article-list">
