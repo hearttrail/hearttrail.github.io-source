a70701d8a4a20d656edaad885bf639c801cd8fe8
Nic Gordon <nic.gordon@inlight.com.au>
2018-12-10 23:43:39 +1100

    Add content block component

---
+++ src/components/content-block/index.js
@@ -0,0 +1,20 @@
+import classNames from 'classnames';
+import React from 'react';
+
+import styles from './styles.module.scss';
+
+export default ({ children, image }) => (
+  <div
+    className={classNames(styles.contentBlock, {
+      [styles.imageLeft]: image.align === 'left',
+    })}
+  >
+    <div className={styles.contentContainer}>
+      <div className={styles.content}>{children}</div>
+    </div>
+    <div
+      className={styles.imageContainer}
+      style={{ backgroundImage: `url(${image.src}` }}
+    />
+  </div>
+);
