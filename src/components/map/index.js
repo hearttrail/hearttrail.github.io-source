ad7c0dfb6d9a8426c14f633279b6f7f9e738c537
Nic Gordon <nic.gordon@inlight.com.au>
2018-12-10 22:42:49 +1100

    Add map component

---
+++ src/components/map/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default () => (
+  <iframe
+    src="https://snazzymaps.com/embed/119288"
+    width="100%"
+    height="400px"
+    style={{ border: 'none' }}
+  />
+);
