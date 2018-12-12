fcd062bd60ad181479ce0d1e479be4afe09ad9dd
Nic Gordon <nic.gordon@inlight.com.au>
2018-12-13 08:40:44 +1100

    Simplify navigation a lot

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -1,116 +1,24 @@
-import classNames from 'classnames';
 import Link from 'gatsby-link';
-import React, { PureComponent } from 'react';
+import React from 'react';
 
-import Hamburger from '../hamburger';
-import logoDark from './images/logo-dark.svg';
-import logoLight from './images/logo-light.svg';
+import constants from '../../constants';
+import CtaButton from '../cta-button';
 import styles from './styles.module.scss';
 
-const NAV_HEIGHT = 60;
-export default class Navigation extends PureComponent {
-  constructor(props) {
-    super(props);
+import HeartLogoImage from '../../images/heart-logo.png';
 
-    this.state = {
-      isMenuOpen: false,
-      isTransparent: true,
-      viewportHeight: 200,
-    };
+export default () => (
+  <nav role="navigation" className={styles.navigation}>
+    <Link className={styles.logoContainer} to="/">
+      <img
+        className={styles.logo}
+        src={HeartLogoImage}
+        alt="Heart Trail Logo"
+      />
+    </Link>
 
-    this.handleResize = this.handleResize.bind(this);
-    this.handleScroll = this.handleScroll.bind(this);
-    this.toggleMenu = this.toggleMenu.bind(this);
-  }
-
-  componentDidMount() {
-    this.handleResize();
-    window.addEventListener('scroll', this.handleScroll);
-    window.addEventListener('resize', this.handleResize);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('scroll', this.handleScroll);
-    window.removeEventListener('resize', this.handleResize);
-  }
-
-  handleResize() {
-    if (!window) {
-      return;
-    }
-
-    this.setState({
-      viewportHeight: window.innerHeight,
-    });
-  }
-
-  handleScroll() {
-    if (!window) {
-      return;
-    }
-
-    const { isTransparent, viewportHeight } = this.state;
-
-    if (window.pageYOffset >= viewportHeight - NAV_HEIGHT && isTransparent) {
-      this.setState({
-        isTransparent: false,
-      });
-    } else if (
-      window.pageYOffset < viewportHeight - NAV_HEIGHT &&
-      !isTransparent
-    ) {
-      this.setState({
-        isTransparent: true,
-      });
-    }
-  }
-
-  toggleMenu() {
-    this.setState(state => ({
-      isMenuOpen: !state.isMenuOpen,
-    }));
-  }
-
-  render() {
-    const { isMenuOpen, isTransparent } = this.state;
-
-    return (
-      <nav
-        role="navigation"
-        className={classNames(styles.navigation, {
-          [styles.isTransparent]: isTransparent && !isMenuOpen,
-        })}
-      >
-        <Hamburger
-          className={styles.hamburger}
-          isActive={isMenuOpen}
-          isDark={isMenuOpen || !isTransparent}
-          onClick={this.toggleMenu}
-        />
-        <div className={styles.logoContainer}>
-          <Link className={classNames(styles.dark, styles.logo)} to="/">
-            <img src={logoDark} />
-          </Link>
-          <Link className={classNames(styles.light, styles.logo)} to="/">
-            <img src={logoLight} />
-          </Link>
-        </div>
-
-        <ul
-          className={classNames(styles.menu, { [styles.isActive]: isMenuOpen })}
-        >
-          <li className={styles.menuItem}>
-            <Link className={styles.menuLink} to="/where">
-              Where
-            </Link>
-          </li>
-          <li className={styles.menuItem}>
-            <Link className={styles.menuLink} to="/news">
-              News
-            </Link>
-          </li>
-        </ul>
-      </nav>
-    );
-  }
-}
+    <CtaButton href={constants.DONOR_REGISTER_URL}>
+      Join the donor register
+    </CtaButton>
+  </nav>
+);
